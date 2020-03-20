75f974136636ddc17cba1cac692997be9c113e44
Christopher Miller <millercs624@gmail.com>
2020-03-19 23:10:47 -0400

    load commands from directory

---
+++ src/commands/play.js
@@ -0,0 +1,10 @@
+//@ts-check
+module.exports = {
+  name: 'play',
+  description: 'Plays the audio of a youtube video',
+  /**
+   * @param {import('discord.js').Message} message
+   * @param {any} args
+   */
+  execute(message, args) {}
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+//@ts-check
 require('dotenv').config();
 const _ = require('lodash/fp');
 const Pino = require('pino');
 const Discord = require('discord.js');
+const fs = require('fs');
 
 const __DEV__ = _.eq(process.env.NODE_ENV, 'development');
 
@@ -10,6 +12,14 @@ const logger = Pino({
 });
 
 const client = new Discord.Client();
+const commandCollection = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands');
+
+for (let file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commandCollection.set(command.name, command);
+}
 
 let prefix = '!';
 
